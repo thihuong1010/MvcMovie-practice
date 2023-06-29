1bfbec5541af1b608324bfc1dc372c4d3854ddf3
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-06-29 21:50:21 +0300

    Change layout file
    Change the title, footer, and menu link

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -6,9 +6,10 @@ namespace MvcMovie.Controllers;
 public class HelloWorldController : Controller
 {
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        // Call the controller View method
+        return View();
     }
     // GET: /HelloWorld/Welcome/
     public string Welcome(string name, int ID = 1)
