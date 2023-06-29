c1ebccbebad0445133248e62526e8f13ba423ab0
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-06-29 23:02:57 +0300

    Passing data from controller to view
    Using ViewData dictionary

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -12,8 +12,10 @@ public class HelloWorldController : Controller
         return View();
     }
     // GET: /HelloWorld/Welcome/
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
