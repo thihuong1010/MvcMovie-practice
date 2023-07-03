c17144c1e990721757caa26fc8002427bd63947c
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-07-03 23:04:42 +0300

    Add Genre search

---
+++ MvcMovie/Models/MovieGereViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace MvcMovie.Models
+{
+	public class MovieGereViewModel
+	{
+		public List<Movie>? Movies { get; set; } // List of movies
+		public SelectList? Genres { get; set; } // Contain list of genres
+		public string? MovieGenre { get; set; } // The selected genre
+		public string? SearchString { get; set; } // User input
+	}
+}
+
