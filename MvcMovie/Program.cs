796cf5a69a3d2c10e82a4c8985caf0f975703fa5
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-07-05 21:08:35 +0300

    Revert to original

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -2,29 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-});
-
+    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ??
+    throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
-
 // Add the seed initializer
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
     SeedData.Initialize(services);
 }
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -32,17 +22,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
-
